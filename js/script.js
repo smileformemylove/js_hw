d7d41e550699cbe1656a6954f4f923d469284d09
Mykola <Shekezzz@gmail.com>
2024-08-01 21:03:37 +0300

    hw_3 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+let  firstNumber = parseFloat(prompt("Введіть перше число:"));
+let  secondNumber = parseFloat(prompt("Введіть друге число:"));
+
+console.log(`Результат: ${firstNumber} + ${secondNumber} =`, firstNumber + secondNumber)
+console.log(`Результат: ${firstNumber} - ${secondNumber} =`, firstNumber - secondNumber)
+console.log(`Результат: ${firstNumber} * ${secondNumber} =`, firstNumber * secondNumber)
+console.log(`Результат: ${firstNumber} / ${secondNumber} =`, firstNumber / secondNumber)
+
+
