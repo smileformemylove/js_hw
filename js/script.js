26e5a5b5bb940da9ebb9c8c488b7b4792ee005d7
Mykola <Shekezzz@gmail.com>
2024-08-01 20:31:20 +0300

    hw_2 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+// Task #1
+let admin = "admin";
+let name = "John";
+admin = name;
+alert(` ${admin}`);
+
+// Task #2
+let planetName = "";
+planetName = "Mars"
+console.log(planetName)
+
+let userName = prompt("Будь ласка, введіть своє ім'я:");
+console.log("Ім'я відвідувача: " + userName);
+
+// Task #3
+let sum = "10" + 10;
+console.log(sum);
+
