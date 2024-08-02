8895b9f329a11ba9664c809b7681660327740062
Mykola <Shekezzz@gmail.com>
2024-08-02 14:18:19 +0300

    hw_3 fix let to const

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let  firstNumber = parseFloat(prompt("Введіть перше число:"));
-let  secondNumber = parseFloat(prompt("Введіть друге число:"));
+const firstNumber = parseFloat(prompt("Введіть перше число:"));
+const secondNumber = parseFloat(prompt("Введіть друге число:"));
 
 console.log(`Результат: ${firstNumber} + ${secondNumber} =`, firstNumber + secondNumber)
 console.log(`Результат: ${firstNumber} - ${secondNumber} =`, firstNumber - secondNumber)
