aceeaf275e8697d1c49ae32b7ec3c24e3eacdca2
Mykola <Shekezzz@gmail.com>
2024-08-07 17:52:21 +0300

    hw_7 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 'use strict';
 
+const hours = Number(prompt("Введіть скільки годин ви хотіли б перевести в секунди: "));
+const seconds = hours * 3600;
+
+alert(`${hours} годин/и = ${seconds} секунд/и`)
