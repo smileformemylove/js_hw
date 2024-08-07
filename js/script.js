b50d56d857ec3847d7791856c9ab2a5b3a7535b3
Mykola <Shekezzz@gmail.com>
2024-08-07 17:35:40 +0300

    hw_8 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 'use strict';
 
+const userNumber = Number(prompt("Введіть перше число: "));
+const userNumber2 = Number(prompt("Введіть друге число: "));
+const userNumber3 = Number(prompt("Введіть третє число: "));
+
+const arithmetic = ((userNumber + userNumber2 + userNumber3) / 3);
+
+alert(`Середнє арифметичне ваших числ буде: ${arithmetic}`);
+
